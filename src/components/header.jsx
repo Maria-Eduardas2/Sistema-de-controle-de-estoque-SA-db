333a47a4f0b29de59909023ceba508dd638e0783
Duda <mariaeduardasm2501@gmail.com>
2025-10-23 00:37:57 -0300

    comecando a fazer a navbar

---
+++ src/components/header.jsx
@@ -0,0 +1,6 @@
+function Appheader() {
+    return <div className="h-[50px] flex justify-between items-center p-1 border-b border-black/15 bg-[#345DBD]"> 
+        AppHeader
+    </div>
+}
+export default Appheader;
